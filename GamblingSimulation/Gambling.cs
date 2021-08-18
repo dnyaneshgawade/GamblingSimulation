7e6dfbcbd03aad996b0dc713296155fe240ff734
dnyana <dnyaneshgawade903@gmail.com>
2021-08-18 09:27:58 +0530

    [Dnyanesh] Add : Starting of Gambling

---
+++ GamblingSimulation/Gambling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingSimulation
+{
+    public class Gambling
+    {
+        public double stack = 100;
+        public int bet = 1;
+        public void Display()
+        {
+            Console.WriteLine("\nStack You Have : "+stack+"\nBet Per Game : "+bet);
+        }
+    }
+}
---
+++ GamblingSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GamblingSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Gambling Simulation!");
+            Gambling gambling = new Gambling();
+            gambling.Display();
+
+        }
+    }
+}
